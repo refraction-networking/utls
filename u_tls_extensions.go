980496f778d72b0998fc0a6ad36bd9672bcf643e
Maxb <bittmanmax@gmail.com>
2020-12-02 15:56:54 -0800

    Implement fix of NextProtoNeg handling in conjunction with NextProtos/ALPN

--- u_tls_extensions.go
+++ u_tls_extensions.go
@@ -242,7 +242,6 @@ type ALPNExtension struct {
 func (e *ALPNExtension) writeToUConn(uc *UConn) error {
 	uc.config.NextProtos = e.AlpnProtocols
 	uc.HandshakeState.Hello.AlpnProtocols = e.AlpnProtocols
-	uc.HandshakeState.Hello.NextProtoNeg = uc.HandshakeState.Hello.NextProtoNeg || len(e.AlpnProtocols) > 0
 	return nil
 }
 
