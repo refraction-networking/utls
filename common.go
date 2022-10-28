82a8255c3e51beb4caec6100a629205f8d2bf448
Gaukas Wang <i@gauk.as>
2022-10-28 00:02:30 -0600

    fix: resolve merge incompatibilities (#7)
    - Removed anything only build with tag `boringcrypto`
    - Defined dummy Boring struct to supply statically defined Boring ENV VAR(s) and functions.
    - Adding back removed `extensionNextProtoNeg` extension.

--- common.go
+++ common.go
@@ -100,6 +100,7 @@ const (
 	extensionCertificateAuthorities  uint16 = 47
 	extensionSignatureAlgorithmsCert uint16 = 50
 	extensionKeyShare                uint16 = 51
+	extensionNextProtoNeg            uint16 = 13172 // not IANA assigned // Pending discussion on whether or not remove this. crypto/tls removed it on Nov 21, 2019.  
 	extensionRenegotiationInfo       uint16 = 0xff01
 )
 
--- notboring.go
+++ notboring.go
@@ -1,10 +1,12 @@
 // Copyright 2022 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-//go:build !boringcrypto
-
 package tls
+
+import (
+	"crypto/cipher"
+	"errors"
+)
 
 func needFIPS() bool { return false }
 
@@ -18,3 +20,20 @@ func fipsCurvePreferences(c *Config) []CurveID { panic("fipsCurvePreferences") }
 func fipsCipherSuites(c *Config) []uint16      { panic("fipsCipherSuites") }
 
 var fipsSupportedSignatureAlgorithms []SignatureScheme
+
+// [uTLS]
+// Boring struct is only to be used to record static env variables
+// in boring package. We do not implement BoringSSL compatibliity here.
+type Boring struct {
+	Enabled bool
+}
+
+func (*Boring) NewGCMTLS(_ cipher.Block) (cipher.AEAD, error) {
+	return nil, errors.New("boring not implemented")
+}
+
+func (*Boring) Unreachable() {
+	// do nothing
+}
+
+var boring Boring
