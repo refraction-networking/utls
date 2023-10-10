e89d82cd6912f53e553d3e984d2ecf97c26355c0
Gaukas Wang <i@gauk.as>
2023-10-09 21:02:26 -0600

    improvement: maintenance+cleanup+fix (#252)
    * ci: use latest Go 1.21 build
    
    Use `1.21.x` instead of `1.21.0` to automatically select the latest.
    
    * fix: remove unused fipsonly package
    
    Remove an unused package that was unintendedly introduced as a conditional dependency of upstream
    
    * update: use boring package not global var
    
    Align with the upstream to use `boring` as a name for a package. No functional changes.
    
    * new: name aliasing
    
    Create u_alias.go to hold any alias names created by version upgrades or other necessary changes (e.g., upstream breaking change) to prevent further breaking the API.

---
+++ internal/boring/notboring.go
@@ -0,0 +1,16 @@
+package boring
+
+import (
+	"crypto/cipher"
+	"errors"
+)
+
+const Enabled bool = false
+
+func NewGCMTLS(_ cipher.Block) (cipher.AEAD, error) {
+	return nil, errors.New("boring not implemented")
+}
+
+func Unreachable() {
+	// do nothing
+}
