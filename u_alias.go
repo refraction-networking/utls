b4873b0030be6f109ffd6e7e40d3bce71cd58d6a
Gaukas Wang <i@gauk.as>
2023-10-09 20:56:51 -0600

    new: name aliasing
    Create u_alias.go to hold any alias names created by version upgrades or other necessary changes (e.g., upstream breaking change) to prevent further breaking the API.

---
+++ u_alias.go
@@ -0,0 +1,12 @@
+package tls
+
+// This file contains all the alias functions, symbols, names, etc. that
+// was once used in the old version of the library.  This is to ensure
+// backwards compatibility with the old version of the library.
+
+// TLS Extensions
+
+// UtlsExtendedMasterSecretExtension is an alias for ExtendedMasterSecretExtension.
+//
+// Deprecated: Use ExtendedMasterSecretExtension instead.
+type UtlsExtendedMasterSecretExtension = ExtendedMasterSecretExtension
