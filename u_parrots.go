be0c457a4b7fefc3c86e191f583de68b11d34440
nihiloid <nihiloid@gmail.com>
2025-04-28 23:24:00 +1000

    u_parrots.go: Minumum randomized TLS version 1.2
    Signed-off-by: nihiloid <nihiloid@gmail.com>

--- u_parrots.go
+++ u_parrots.go
@@ -2993,7 +2993,7 @@ func generateRandomizedSpec(
 	}
 
 	if r.FlipWeightedCoin(id.Weights.TLSVersMax_Set_VersionTLS13) {
-		p.TLSVersMin = VersionTLS10
+		p.TLSVersMin = VersionTLS12
 		p.TLSVersMax = VersionTLS13
 		tls13ciphers := make([]uint16, len(defaultCipherSuitesTLS13))
 		copy(tls13ciphers, defaultCipherSuitesTLS13)
