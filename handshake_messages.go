8464bdde124bf39865a0acf01074acecdbe07c94
Juktong <matseoi@unc.edu>
2025-06-01 02:16:45 -0400

    feat: Implement Certificate Compression Algorithm

--- handshake_messages.go
+++ handshake_messages.go
@@ -1280,6 +1280,7 @@ type certificateRequestMsgTLS13 struct {
 	scts                             bool
 	supportedSignatureAlgorithms     []SignatureScheme
 	supportedSignatureAlgorithmsCert []SignatureScheme
+	certRequestCompressionAlgs       []CertCompressionAlgo
 	certificateAuthorities           [][]byte
 }
 
@@ -1315,6 +1316,17 @@ func (m *certificateRequestMsgTLS13) marshal() ([]byte, error) {
 					})
 				})
 			}
+			if len(m.certRequestCompressionAlgs) > 0 {
+				b.AddUint16(extensionCompressCertificate)
+				b.AddUint16LengthPrefixed(func(b *cryptobyte.Builder) {
+					b.AddUint8LengthPrefixed(func(b *cryptobyte.Builder) {
+						for _, algo := range m.certRequestCompressionAlgs {
+							b.AddUint16(uint16(algo))
+						}
+					})
+				})
+			}
+
 			if len(m.supportedSignatureAlgorithmsCert) > 0 {
 				b.AddUint16(extensionSignatureAlgorithmsCert)
 				b.AddUint16LengthPrefixed(func(b *cryptobyte.Builder) {
@@ -1380,6 +1392,19 @@ func (m *certificateRequestMsgTLS13) unmarshal(data []byte) bool {
 				}
 				m.supportedSignatureAlgorithms = append(
 					m.supportedSignatureAlgorithms, SignatureScheme(sigAndAlg))
+			}
+		case extensionCompressCertificate:
+			var algs cryptobyte.String
+			if !extData.ReadUint8LengthPrefixed(&algs) || algs.Empty() {
+				return false
+			}
+			for !algs.Empty() {
+				var alg uint16
+				if !algs.ReadUint16(&alg) {
+					return false
+				}
+				m.certRequestCompressionAlgs = append(
+					m.certRequestCompressionAlgs, CertCompressionAlgo(alg))
 			}
 		case extensionSignatureAlgorithmsCert:
 			var sigAndAlgs cryptobyte.String
